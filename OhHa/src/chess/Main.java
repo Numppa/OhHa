9edae334b6e3f562ccfee628b8da9b3fa5c30cf1
Joel <joel@joel-305U1A.(none)>
2012-12-17 20:42:56 +0200

    käyttöliittymää

--- OhHa/src/chess/Main.java
+++ OhHa/src/chess/Main.java
@@ -1,8 +1,12 @@
 package chess;
 
+import chess.ui.Ui;
+import javax.swing.SwingUtilities;
+
 public class Main {
 
     public static void main(String[] args) {
-        
+        Ui ui = new Ui();
+        SwingUtilities.invokeLater(ui);
     }
 }
--- OhHa/src/chess/ui/Ui.java
+++ OhHa/src/chess/ui/Ui.java
@@ -3,7 +3,11 @@ package chess.ui;
 
 import java.awt.Container;
 import java.awt.Dimension;
+import java.awt.FlowLayout;
+import java.awt.GridLayout;
+import javax.swing.JButton;
 import javax.swing.JFrame;
+import javax.swing.JPanel;
 import javax.swing.WindowConstants;
 
 public class Ui implements Runnable{
@@ -16,7 +20,7 @@ public class Ui implements Runnable{
     @Override
     public void run() {
         frame = new JFrame("Joel's Chess");
-        frame.setPreferredSize(new Dimension(800 , 500));
+        frame.setPreferredSize(new Dimension(1200 , 600));
         
         frame.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
         
@@ -27,7 +31,17 @@ public class Ui implements Runnable{
     }
 
     public void createComponents(Container container) {
+        GridLayout layout = new GridLayout(1 , 2);
+        container.setLayout(layout);
         
+        GridLayout grids = new GridLayout(8, 8);
+        JPanel panel = new JPanel(grids);
+        
+        for (int i = 0; i < 64; i++) {
+            panel.add(new JButton());
+        }
+        container.add(panel);
+        container.add(new JButton());
         
     }
     
