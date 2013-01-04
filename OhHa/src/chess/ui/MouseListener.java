690bae649c90e8ea0759f9f9278956e41c63db0a
Joel <joel@joel-305U1A.(none)>
2013-01-04 22:27:40 +0200

    korjaus

--- OhHa/src/chess/ui/MouseListener.java
+++ OhHa/src/chess/ui/MouseListener.java
@@ -1,13 +1,32 @@
-/*
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
- */
 package chess.ui;
 
-/**
- *
- * @author joel
- */
-public class MouseListener {
+import java.awt.event.MouseEvent;
+
+public class MouseListener implements java.awt.event.MouseListener{
+
+    @Override
+    public void mouseClicked(MouseEvent arg0) {
+        throw new UnsupportedOperationException("Not supported yet.");
+    }
+
+    @Override
+    public void mousePressed(MouseEvent arg0) {
+        throw new UnsupportedOperationException("Not supported yet.");
+    }
+
+    @Override
+    public void mouseReleased(MouseEvent arg0) {
+        throw new UnsupportedOperationException("Not supported yet.");
+    }
+
+    @Override
+    public void mouseEntered(MouseEvent arg0) {
+        throw new UnsupportedOperationException("Not supported yet.");
+    }
+
+    @Override
+    public void mouseExited(MouseEvent arg0) {
+        throw new UnsupportedOperationException("Not supported yet.");
+    }
     
 }
